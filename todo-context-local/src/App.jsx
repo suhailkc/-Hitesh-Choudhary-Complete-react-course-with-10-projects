1a12d062fa04d5708237a6b3cb0c2cf5f9a5cbf4
SUHAIL KC <suhailkc@MacBooks-MacBook-Pro.local>
2024-12-23 14:19:36 +0530

    minor error fix

--- todo-context-local/src/App.jsx
+++ todo-context-local/src/App.jsx
@@ -19,9 +19,7 @@ function App() {
 
   const toggleComplete = (id) => {
     setTodos((prev) => {
-      prev.map((prevTodo) => {
-        prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo
-      })
+      return prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo )
     })
   }
 
